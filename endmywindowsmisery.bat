769097b0e9de39b8a5a4dd2f61c592ea94ea608b
Rip_Dead <ripdead890@gmail.com>
2025-09-16 15:31:34 +0300

    Add files via upload

---
+++ endmywindowsmisery.bat
@@ -0,0 +1,111 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFD54aAuBKGSoOpET/+b34OuDsXEbVfQ2e4DVlL2NL4A=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFD54aAuBKGSoOpET/+b34OuDsXE4WuZ/VZab7b2AJO8E+QXJfIUoxGMUndMJbA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title End My Windows Misery (FAKE APP)
+
+echo Hello, Welcome to End My Windows Misery (this doesnt delete anything)
+echo.
+echo To start, type delete-serverfile
+echo.
+echo This will give us perms to delete server files Microsoft doesn't want to let us delete
+echo.
+
+:: Fake input prompt
+set /p userinput="Type command here: "
+echo.
+echo Starting "server file removal"...
+timeout /t 2 >nul
+echo 1 Deleting encrypted server file... ██████████
+echo 2 Deleting encrypted server file... ██████████
+echo 1 Deleting encrypted server file... ██████████
+echo 4 Deleting encrypted server file... ██████████
+echo 5 Deleting encrypted server file... ██████████
+echo 6 Deleting encrypted server file... ██████████
+echo 7 Deleting encrypted server file... ██████████
+echo 8 Deleting encrypted server file... ██████████
+echo 9 Deleting encrypted server file... ██████████
+echo 10 Deleting encrypted server file... ██████████
+echo 11 Deleting encrypted server file... ██████████
+echo 12 Deleting encrypted server file... ██████████
+echo 13 Deleting encrypted server file... ██████████
+timeout /t 1 >nul
+echo 14 Deleting encrypted server file... ██████████
+echo 15 Deleting encrypted server file... ██████████
+echo 16 Deleting encrypted server file... ██████████
+echo 17 Deleting encrypted server file... ██████████
+echo 18 Deleting encrypted server file... ██████████
+echo 19 Deleting encrypted server file... ██████████
+echo 20 Deleting encrypted server file... ██████████
+echo 21 Deleting decrypted server file... [shell32.dll]
+echo 22 Deleting decrypted server file... [sysWOW64]
+echo 23 Deleting encrypted server file... ██████████
+echo 24 Deleting encrypted server file... ██████████
+echo 25 Deleting encrypted server file... ██████████
+echo 26 Deleting encrypted server file... ██████████
+echo 27 Deleting decrypted server file... [system32]
+echo 28 Deleting decrypted server file... [Users]
+echo 29 Deleting encrypted server file... [winlogon.exe]
+echo 30 Deleting encrypted server file... ██████████
+echo 31 Deleting encrypted server file... ██████████
+echo 32 Deleting encrypted server file... ██████████
+echo 33 Deleting encrypted server file... ██████████
+echo 34 Deleting encrypted server file... ██████████
+echo 35 Deleting encrypted server file... ██████████
+echo 36 Deleting encrypted server file... ██████████
+timeout /t 3 >nul
+echo 37 Deleting encrypted server file... ██████████
+echo 38 Deleting encrypted server file... ██████████
+echo 39 Deleting encrypted server file... ██████████
+echo 40 Deleting encrypted server file... ██████████
+echo 41 Deleting encrypted server file... ██████████
+echo 42 Deleting encrypted server file... ██████████
+timeout /t 1 >nul
+echo 43 Deleting decrypted server file... [Program Files x86]
+echo 44 Deleting encrypted server file... ██████████
+echo 45 Deleting encrypted server file... ██████████
+echo 46 Deleting encrypted server file... ██████████
+echo 47 Deleting encrypted server file... ██████████
+echo 48 Deleting encrypted server file... ██████████
+echo 49 Deleting encrypted server file... ██████████
+echo 50 Deleting encrypted server file... ██████████
+echo 51 Deleting encrypted server file... ██████████
+echo 52 Deleting encrypted server file... ██████████
+echo 53 Deleting encrypted server file... ██████████
+echo 54 Deleting encrypted server file... ██████████
+echo 55 Deleting encrypted server file... ██████████
+echo 56 Deleting encrypted server file... ██████████
+timeout /t 5 >nul
+echo .
+echo 56 server files removed. Your PC is now dead (not actually)!
+pause
